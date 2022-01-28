1186e9fb0239e71cb86f0d96c72dea70d7f730ef
Mouez98 <mouez.matoui@gmail.com>
2022-01-28 18:12:43 +0100

    Changed dummy to fetched data

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,17 +1,47 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-import { DUMMY_MEALS } from "./DummyMeals";
+import Loading from '../../imported/loading'
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealList = DUMMY_MEALS.map((item) => (
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-94c27-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const data = await response.json();
+      let meals = []; 
+      for (const key in data) {
+
+        meals.push({
+          id: key,
+          name: data[key].name,
+          price: data[key].price,
+          desc: data[key].desc,
+        });
+      }
+      setMeals(meals);
+      setLoading(false)
+    };
+    fetchMeals()
+  }, []);
+  
+  if(loading) {
+    return <section> 
+     <Loading />
+    </section>
+  }
+
+  const mealList = meals.map((item) => (
     <MealItem key={item.id} {...item} item={item} />
   ));
   return (
     <Card className={classes.meals}>
-      <ul>
-        {mealList}
-      </ul>
+      <ul>{mealList}</ul>
     </Card>
   );
 };
