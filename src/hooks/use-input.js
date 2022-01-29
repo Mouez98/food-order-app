4692c0e2ac6c3e2c40a75efa50d4524b4b0f405d
Mouez98 <mouez.matoui@gmail.com>
2022-01-29 15:26:56 +0100

    edits on valididty

---
+++ src/hooks/use-input.js
@@ -0,0 +1,44 @@
+import { useReducer } from "react";
+
+const inputHelper = {
+    value: '',
+    isTouched: false
+}
+
+const inputReducer = (state, action) => {
+    if(action.type === 'INPUT'){
+        return {value: action.value, isTouched: state.isTouched }
+    }
+
+    if(action.type === 'BLUR'){
+        return {value: state.value, isTouched: true}
+    }
+
+    if(action.type === 'RESET'){
+        return {value: '', isTouched: false}
+    }
+   return inputReducer
+}
+
+
+const useInput = validitiy => {
+const [valueState, dispatch] = useReducer(inputReducer, inputHelper);
+
+const isValid = validitiy(valueState.value)
+const hasError = !isValid && valueState.isTouched;
+
+const onChangeHandler = e => dispatch({type: 'INPUT', value: e.target.value});
+const onBlurHandler = e => dispatch({type: 'BLUR'});
+const reset = () => dispatch({type: 'RESET'});
+
+return {
+    value: valueState.value,
+    hasError,
+    onChangeHandler,
+    onBlurHandler,
+    reset
+}
+
+}
+
+export default useInput;
